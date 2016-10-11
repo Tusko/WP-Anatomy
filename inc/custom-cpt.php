6ddaffc74919027afdd8770321bf004eaef983aa
Tusko Trush <tusko@photoinside.me>
2016-10-11 20:32:06 +0300

    wrkd

--- inc/custom-cpt.php
+++ inc/custom-cpt.php
@@ -73,3 +73,42 @@ function register_cpts() {
     }
 
 }
+
+function workd_restrict_manage_posts() {
+    global $typenow;
+    $args=array( 'public' => true, '_builtin' => false );
+    $post_types = get_post_types($args);
+    if ( in_array($typenow, $post_types) ) {
+    $filters = get_object_taxonomies($typenow);
+        foreach ($filters as $tax_slug) {
+            $tax_obj = get_taxonomy($tax_slug);
+            wp_dropdown_categories(array(
+                'show_option_all' => __('Filter -> '.$tax_obj->label ),
+                'taxonomy' => $tax_slug,
+                'name' => $tax_obj->name,
+                'orderby' => 'term_order',
+                'selected' => $_GET[$tax_obj->query_var],
+                'hierarchical' => $tax_obj->hierarchical,
+                'show_count' => false,
+                'hide_empty' => true
+            ));
+        }
+    }
+}
+function workd_convert_restrict($query) {
+    global $pagenow;
+    global $typenow;
+    if ($pagenow=='edit.php') {
+        $filters = get_object_taxonomies($typenow);
+        foreach ($filters as $tax_slug) {
+            $var = &$query->query_vars[$tax_slug];
+            if ( isset($var) && $var>0) {
+                $term = get_term_by('id',$var,$tax_slug);
+                $var = $term->slug;
+            }
+        }
+    }
+    return $query;
+}
+add_action('restrict_manage_posts', 'workd_restrict_manage_posts');
+add_filter('parse_query', 'workd_convert_restrict');

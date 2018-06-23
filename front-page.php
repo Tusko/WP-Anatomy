28d2220cb9db92d0a0a562c6064e5cc6d70914fb
Tusko <tusko@photoinside.me>
2018-06-23 10:42:44 +0300

    GF added checkbox/radio buttons styles

--- front-page.php
+++ front-page.php
@@ -4,8 +4,7 @@
         <img src="<?php echo wpa_placeholder(get_post_thumbnail_id($post->ID), 'full'); ?>" data-defer="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full') ?>" alt="<?php echo get_alt(get_post_thumbnail_id($post->ID)); ?>" />
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
             the_content();
-        endwhile; endif;
-        ?>
+        endwhile; endif; ?>
     </article>
     <aside>
     </aside>

b6c4c8b8a4ee1810170d5933bad49097bd81a260
Tusko <tusko@photoinside.me>
2019-02-11 12:18:00 +0200

    php 7.3 compatibility

--- inc/wpadmin/admin-addons.php
+++ inc/wpadmin/admin-addons.php
@@ -185,8 +185,8 @@ add_filter( 'show_admin_bar', '__return_false' ); // remove Admin Bar
 
 // Remove the wordpress update notifications for all users except Super Administator
 if (!current_user_can('update_plugins')) { // checks to see if current user can update plugins
-    add_action( 'init', create_function( '$a', "remove_action( 'init', 'wp_version_check' );" ), 2 );
-    add_filter( 'pre_option_update_core', create_function( '$a', "return null;" ) );
+	add_action( 'init', function(){ remove_action( 'init', 'wp_version_check' ); }, 2 );
+	add_filter( 'pre_option_update_core', function($a){return null;} );
 }
 
 // Return header 403 for wrong login

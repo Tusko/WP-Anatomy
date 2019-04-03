7168fc50f7a882bd7f1e6370aa68be788c1bfa66
Tusko <tusko@photoinside.me>
2019-04-03 12:38:01 +0300

    lazysizes added

---
+++ js/libs/arsmoon.console.js
@@ -0,0 +1,1 @@
+console.log("Handcrafted by\n\n%c\www.arsmoon.com\n%c", "padding: 0px; color:#000000; line-height:30px; font-size: 18px; font-family: georgia, serif; font-style: italic;", "padding: 0px; color:#000000; line-height:20px; font-size: 12px; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen-Sans', 'Ubuntu', 'Cantarell', 'Helvetica Neue', sans-serif;");

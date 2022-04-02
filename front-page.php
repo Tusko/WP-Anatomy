4a9334e5f81df12f8400f34627a6ce5e1789e230
Tusko <tusko@photoinside.me>
2022-04-02 15:04:10 +0300

    upd libs

--- front-page.php
+++ front-page.php
@@ -1,19 +1,15 @@
 <?php get_header();
 global $post; ?>
 	<section class="content row">
-
-		<h1>test 1 2 3 4 5 5 5 5333</h1>
-		<h2>test cache 224</h2>
 		<article class="animatedParent animatedOnce">
 			<?php the_post_thumbnail('full', array(
-				'class' => 'animated fadeInUp ac'
+				'data-aos'      => 'fade-up',
+				'data-fancybox' => ''
 			)); ?>
 
 			<?php if(have_posts()) : while(have_posts()) : the_post();
 				the_content();
 			endwhile; endif; ?>
-			<p><a href="#">Hello world</a></p>
-			<p><a href="#" class="button">Hello world</a></p>
 		</article>
 		<aside>
 		</aside>
